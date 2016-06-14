f802f0f76bee4bb0bdf0914c7f173d1e8ced7509
DPL2016 <dingpengliang@aliyun.com>
2016-06-14 11:02:16 +0800

    文件md5测试

--- servlettest/src/com/kaishengit/test/CodecTest.java
+++ servlettest/src/com/kaishengit/test/CodecTest.java
@@ -3,23 +3,36 @@ package com.kaishengit.test;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class CodecTest {
     //md5测试
     @Test
-    public void codecTest(){
+    public void codecTest() {
         String password = "1231232";
         String salt = "qwerirux";
-        password = DigestUtils.md5Hex(password+salt);
+        password = DigestUtils.md5Hex(password + salt);
         System.out.println(password);
 
     }
 
     //sha-1 测试
-    public void  shaTest(){
+    @Test
+    public void shaTest() {
         String password = "1231232";
         String salt = "qowejiewop";
-        password = DigestUtils.sha1Hex(password+salt);
+        password = DigestUtils.sha1Hex(password + salt);
         System.out.println(password);
     }
-    
+
+    //文件md5测试
+    @Test
+    public void fileMd5Test() throws Exception {
+        FileInputStream inputStream = new FileInputStream(new File("D:/mylog2.log"));
+        String md5 = DigestUtils.md5Hex(inputStream);
+        System.out.println(md5);
+
+    }
 }
