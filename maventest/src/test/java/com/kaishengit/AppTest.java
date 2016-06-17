df36d18b16adc188bd8d26c17cac09c4b43f9251
DPL2016 <dingpengliang@aliyun.com>
2016-06-17 13:28:23 +0800

    maven 测试

---
+++ maventest/src/test/java/com/kaishengit/AppTest.java
@@ -0,0 +1,13 @@
+package com.kaishengit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest{
+    @Test
+    public void test(){
+        String str = "12133";
+        Assert.assertNotNull(str);
+    }
+}
+
