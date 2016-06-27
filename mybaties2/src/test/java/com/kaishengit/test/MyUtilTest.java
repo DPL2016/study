6dc203e63ef36ab528daeebb138a01341b879a9b
DPL2016 <dingpengliang@aliyun.com>
2016-06-27 21:27:23 +0800

    给MyBatis2添加增删改查

--- mybaties2/src/test/java/com/kaishengit/test/MyUtilTest.java
+++ mybaties2/src/test/java/com/kaishengit/test/MyUtilTest.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class MyUtilTest {
     @Test
     public void TestLoadXml(){
@@ -14,5 +16,46 @@ public class MyUtilTest {
         sqlSession.close();
         System.out.println(admin);
         Assert.assertNotNull(admin);
+    }
+
+    @Test
+    public void saveTest(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        Admin admin = new Admin();
+        admin.setName("小黑");
+        admin.setPassword("12345");
+        admin.setAddress("Usa");
+        sqlSession.insert("com.kaishengit.mapper.AdminMapper.save",admin);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateTest(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        Admin admin = sqlSession.selectOne("com.kaishengit.mapper.AdminMapper.findById",9);
+        admin.setPassword("145123");
+        admin.setAddress("123");
+        sqlSession.update("com.kaishengit.mapper.AdminMapper.update",admin);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteTest(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        sqlSession.delete("com.kaishengit.mapper.AdminMapper.delete",12);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void findAllTest(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        List<Admin> adminList = sqlSession.selectList("com.kaishengit.mapper.AdminMapper.findAll");
+        for (Admin admin :adminList){
+            System.out.println(admin);
+        }
+        sqlSession.close();
     }
 }
