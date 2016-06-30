7b636f6ece4a330e1cc9a97bf74ac16d85952ba2
DPL2016 <dingpengliang@aliyun.com>
2016-06-30 17:50:08 +0800

    构造方法注入，自动注入

--- springtest/src/main/java/com/kaishengit/service/UserService.java
+++ springtest/src/main/java/com/kaishengit/service/UserService.java
@@ -2,11 +2,16 @@ package com.kaishengit.service;
 
 import com.kaishengit.dao.UserDao;
 public class UserService {
-    private UserDao userDao;
 
-    public void setUserDao(UserDao userDao) {
+    public UserService(UserDao userDao) {
         this.userDao = userDao;
     }
+
+    private UserDao userDao;
+
+//    public void setUserDao(UserDao userDao) {
+//        this.userDao = userDao;
+//    }
     public void sayHello(){
         System.out.println("hello,Spring");
         userDao.sayHi();
