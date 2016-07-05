06612f70ab374db6decd31e6f1447691edcea78e
DPL2016 <dingpengliang@aliyun.com>
2016-07-05 19:10:48 +0800

    分页

--- ssm/src/main/java/com/kaishengit/cotroller/BookController.java
+++ ssm/src/main/java/com/kaishengit/cotroller/BookController.java
@@ -5,12 +5,14 @@ import com.kaishengit.pojo.Book;
 import com.kaishengit.pojo.BookType;
 import com.kaishengit.pojo.Publisher;
 import com.kaishengit.service.BookService;
+import com.kaishengit.util.Page;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.inject.Inject;
@@ -23,9 +25,9 @@ public class BookController {
     private BookService bookService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list(Model model){
-        List<Book> bookList = bookService.findAllBook();
-        model.addAttribute("bookList",bookList);
+    public String list(@RequestParam(required = false,defaultValue = "1") Integer p,Model model){
+        Page<Book> page = bookService.findAllBookPage(p);
+        model.addAttribute("page",page);
         return "/books/list";
     }
 
--- ssm/src/main/java/com/kaishengit/service/BookService.java
+++ ssm/src/main/java/com/kaishengit/service/BookService.java
@@ -6,6 +6,7 @@ import com.kaishengit.mapper.PublisherMapper;
 import com.kaishengit.pojo.Book;
 import com.kaishengit.pojo.BookType;
 import com.kaishengit.pojo.Publisher;
+import com.kaishengit.util.Page;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
@@ -44,5 +45,13 @@ public class BookService {
     }
     public Book findById(Integer id){
         return bookMapper.findById(id);
+    }
+
+    public Page<Book> findAllBookPage(Integer p) {
+        int totalSize = bookMapper.count().intValue();
+        Page<Book> page = new Page<>(p,5,totalSize);
+        List<Book> bookList = bookMapper.findByPage(page.getStart(),5);
+        page.setItems(bookList);
+        return page;
     }
 }
